a49cf247126cba0e30f7690dc3bb723ccb72ddde
Terence Alberti <terence.alberti@xmedia.ch>
2016-07-08 00:49:32 +0200

    refactored code

--- CrazyDriver/GameMainView.swift
+++ CrazyDriver/GameMainView.swift
@@ -10,17 +10,6 @@ import UIKit
 
 public class GameMainView: UIView {
     
-    // clock
-    var animationClock : CADisplayLink = CADisplayLink()
-    
-    // models
-    var carModel = CarModel()
-    var sensorModel = SensorsModel()
-    var gameModel = GameModel()
-    
-    // obstacles
-    var obstacles : [(model: ObstacleModel, view: UIImageView)] = []
-    
     // views
     var streetViewArray = Array<UIImageView>()
     @IBOutlet weak var carImageView: UIImageView!
@@ -30,53 +19,15 @@ public class GameMainView: UIView {
         super.draw(rect)
     }
     
-    public func initializeGame(_ obstacles : Array<ObstacleModel>){
-        for obstacle in obstacles{
-            let imageView = UIImageView(image: UIImage(named: obstacle.imageName))
-            imageView.frame.origin.x = CGFloat(obstacle.positionX)
-            imageView.frame.origin.y = CGFloat(obstacle.positionY)
-            self.obstacles.append((model:obstacle, view: imageView))
-        }
-        sensorModel.start()
-        carModel.positionX = Double(self.bounds.size.width/2)
-        initializeStreetViews()
-    }
     
-    // start animation clock
-    public func startAnimationClock(){
-        self.animationClock = CADisplayLink(target: self, selector:#selector(nextCicle))
-        self.animationClock.add(to: RunLoop.current(), forMode: RunLoopMode.defaultRunLoopMode.rawValue)
-        addObstacleViews()
-    }
-    
-    private func addObstacleViews(){
+    public func addObstacleViews(obstacles : [(model : ObstacleModel,view : UIImageView)]){
         for (_, view) in obstacles{
             self.addSubview(view)
         }
     }
     
-    // update UI model before redraw
-    func nextCicle(){
-        updateModel()
-        updateView()
-        setNeedsDisplay()
-    }
     
-    func updateModel(){
-        // update the car position (maintain the car inside the street)
-        
-        let updateCarLeft = self.sensorModel.currentRotationY > 0 ? true :false
-        let howMuch = updateCarLeft ? self.sensorModel.currentRotationY*(-1) : self.sensorModel.currentRotationY
-        updateCarPosition(howMuch*25,left: updateCarLeft)
-        
-        updateCarSpeed()
-        
-        for (model,_) in obstacles{
-            model.positionY += gameModel.speedRelativeToStreet(objectSpeed: model.speedPerTick)
-        }
-    }
-    
-    func updateView(){
+    public func updateView(carModel : CarModel, gameModel : GameModel, obstacles : [(model : ObstacleModel,view : UIImageView)]){
         carImageView.frame.origin.x = CGFloat(carModel.positionX)
         for streetImageView: UIImageView in self.streetViewArray {
             
@@ -97,13 +48,10 @@ public class GameMainView: UIView {
         }
     }
     
-    func updateCarSpeed(){
-        // change car speed with bounds, maxCarSpeed...minCarSpeed
-        if(self.sensorModel.currentRotationX > 0){
-            self.gameModel.carSpeed += (self.gameModel.carSpeed + self.sensorModel.currentRotationX > self.gameModel.maxCarSpeed ? self.gameModel.maxCarSpeed - self.gameModel.carSpeed  : self.sensorModel.currentRotationX)
-        }
-        else{
-            self.gameModel.carSpeed += (self.gameModel.carSpeed + self.sensorModel.currentRotationX < self.gameModel.minCarSpeed ? self.gameModel.carSpeed - self.gameModel.minCarSpeed : self.sensorModel.currentRotationX)
+    func removeObstacleIfNeeded(obstacle: (model: ObstacleModel, view: UIView)){
+        if(obstacle.model.positionY>500){
+            obstacle.view.removeFromSuperview()
+            obstacle.model.destroyed = true
         }
     }
     
@@ -157,36 +105,9 @@ public class GameMainView: UIView {
         streetView.frame.origin.y = CGFloat(newYPosition)
     }
     
-    func streetOriginX() -> CGFloat{
+    public func streetOriginX() -> CGFloat{
         return self.frame.size.width / CGFloat(2) - CGFloat(Constants.streetHeight)/2
     }
     
-    func updateCarPosition(_ howMuch: Double, left: Bool){
-        // car moving to left
-        if(left){
-            if(self.carModel.positionX+howMuch > Double(self.streetOriginX())){
-                self.carModel.positionX += howMuch
-            }
-            else{
-                self.carModel.positionX = Double(self.streetOriginX())
-            }
-        }
-        // car moving to right
-        else if(!left){
-            if(self.carModel.positionX-howMuch < Double(self.streetOriginX()+CGFloat(Constants.streetWidth)-self.carImageView.frame.size.width)){
-                self.carModel.positionX -= howMuch
-            }
-            else{
-                self.carModel.positionX = Double(self.streetOriginX()+CGFloat(Constants.streetWidth)-self.carImageView.frame.size.width)
-            }
-        }
-    }
-    
-    func removeObstacleIfNeeded(obstacle: (model: ObstacleModel, view: UIView)){
-            if(obstacle.model.positionY>500){
-                obstacle.view.removeFromSuperview()
-                obstacle.model.destroyed = true
-            }
-    }
 
 }
--- CrazyDriver/ViewController.swift
+++ CrazyDriver/ViewController.swift
@@ -11,11 +11,27 @@ import CoreMotion
 
 class ViewController: UIViewController {
     
+    // clock
+    var animationClock : CADisplayLink = CADisplayLink()
+    
+    // models
+    var carModel = CarModel()
+    var sensorModel = SensorsModel()
+    var gameModel = GameModel()
+    
+    // views
+    var streetViewArray = Array<UIImageView>()
+    var gameMainView : GameMainView? = nil // is set in viewdidload
+    
+    // obstacles
+    var obstacles : [(model: ObstacleModel, view: UIImageView)] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let gameMainView = self.view as! GameMainView
+        gameMainView = self.view as? GameMainView
         let obstacle = ObstacleModel()
         obstacle.imageName = "ObstacleRedCar"
         obstacle.positionX = 300
@@ -24,8 +40,8 @@ class ViewController: UIViewController {
         var obstacles = Array<ObstacleModel>()
         obstacles.append(obstacle)
         
-        gameMainView.initializeGame(obstacles)
-        gameMainView.startAnimationClock()
+        initializeGame(obstacles)
+        startAnimationClock()
         
     }
 
@@ -33,7 +49,78 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func initializeGame(_ obstacles : Array<ObstacleModel>){
+        for obstacle in obstacles{
+            let imageView = UIImageView(image: UIImage(named: obstacle.imageName))
+            imageView.frame.origin.x = CGFloat(obstacle.positionX)
+            imageView.frame.origin.y = CGFloat(obstacle.positionY)
+            self.obstacles.append((model:obstacle, view: imageView))
+        }
+        sensorModel.start()
+        carModel.positionX = Double(self.view.bounds.size.width/2)
+        let gameMainView = self.view as! GameMainView
+        gameMainView.initializeStreetViews()
+    }
+    
+    // start animation clock
+    func startAnimationClock(){
+        self.animationClock = CADisplayLink(target: self, selector:#selector(nextCicle))
+        self.animationClock.add(to: RunLoop.current(), forMode: RunLoopMode.defaultRunLoopMode.rawValue)
+        gameMainView?.addObstacleViews(obstacles: obstacles)
+    }
+    
+    // update UI model before redraw
+    func nextCicle(){
+        updateModel()
+        gameMainView?.updateView(carModel:carModel,gameModel:gameModel,obstacles:obstacles)
+        gameMainView?.setNeedsDisplay()
+    }
+    
+    func updateModel(){
+        // update the car position (maintain the car inside the street)
+        
+        let updateCarLeft = self.sensorModel.currentRotationY > 0 ? true :false
+        let howMuch = updateCarLeft ? self.sensorModel.currentRotationY*(-1) : self.sensorModel.currentRotationY
+        updateCarPosition(howMuch*25,left: updateCarLeft)
+        updateCarSpeed()
+        
+        for (model,_) in obstacles{
+            model.positionY += gameModel.speedRelativeToStreet(objectSpeed: model.speedPerTick)
+        }
+    }
+    
+    func updateCarSpeed(){
+        // change car speed with bounds, maxCarSpeed...minCarSpeed
+        if(self.sensorModel.currentRotationX > 0){
+            self.gameModel.carSpeed += (self.gameModel.carSpeed + self.sensorModel.currentRotationX > self.gameModel.maxCarSpeed ? self.gameModel.maxCarSpeed - self.gameModel.carSpeed  : self.sensorModel.currentRotationX)
+        }
+        else{
+            self.gameModel.carSpeed += (self.gameModel.carSpeed + self.sensorModel.currentRotationX < self.gameModel.minCarSpeed ? self.gameModel.carSpeed - self.gameModel.minCarSpeed : self.sensorModel.currentRotationX)
+        }
+    }
+    
+    func updateCarPosition(_ howMuch: Double, left: Bool){
+        // car moving to left
+        if(left){
+            if(self.carModel.positionX+howMuch > Double((gameMainView?.streetOriginX())!)){
+                self.carModel.positionX += howMuch
+            }
+            else{
+                self.carModel.positionX = Double((gameMainView?.streetOriginX())!)
+            }
+        }
+            // car moving to right
+        else if(!left){
+            if(self.carModel.positionX-howMuch < Double((gameMainView?.streetOriginX())!+CGFloat(Constants.streetWidth)-(gameMainView?.carImageView.frame.size.width)!)){
+                self.carModel.positionX -= howMuch
+            }
+            else{
+                self.carModel.positionX = Double((gameMainView?.streetOriginX())!+CGFloat(Constants.streetWidth)-(gameMainView?.carImageView.frame.size.width)!)
+            }
+        }
+    }
+    
 
 }
 
